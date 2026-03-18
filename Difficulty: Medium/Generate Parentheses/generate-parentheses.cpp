532d8686785388b00592363483cd87714b6bb9fd
𝖳𝖺𝗋𝖺𝗄𝖺 𝖱𝖺𝗆𝖺𝗋𝖺𝗈 <tarakaramarao0506@gmail.com>
2026-03-18 17:57:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Generate Parentheses/generate-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    vector<string> ans;
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push('(');
+            } else {
+                if (st.empty())
+                    return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+    void dfs(string& curr, int n){
+        if(curr.length() == n){
+            if(isValid(curr)) 
+                ans.emplace_back(curr);
+            return ;
+        }
+        
+        curr.push_back('(');
+        dfs(curr, n);
+        curr.pop_back();
+        
+        curr.push_back(')');
+        dfs(curr, n);
+        curr.pop_back();
+    }
+    vector<string> generateParentheses(int n) {
+        string curr = "";
+        dfs(curr, n);
+        return ans;
+    }
+};
